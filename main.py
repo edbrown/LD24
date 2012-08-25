f22918f83dde7172b1cfd5ac2e797f034289c977
edb <ed.j.brown@googlemail.com>
2012-08-25 13:06:14 +0100

    Added impassable objects

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pyglet
 
 data = [
+    [0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,0,1,0,0,0,0],
+    [0,0,0,0,0,0,0,1,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     ]
 TILE_WIDTH = 64
 TILE_HEIGHT = 32
 OFFSET_Y = 200;
+
+PASSABLE = 0
+IMPASSABLE  = 1
 
 class Game(pyglet.window.Window):
   def __init__(self):
@@ -32,7 +35,12 @@ class Map():
       for j in range(len(data[i])):
         x = (j * TILE_WIDTH / 2) + (i * TILE_WIDTH / 2)
         y = (i * TILE_HEIGHT / 2) - (j * TILE_HEIGHT / 2)
-        self.tiles.append(Tile(self.grid[0,0], x, y))
+        if(data[i][j] == PASSABLE):
+          type = self.grid[1,0]
+        else:
+          type = self.grid[0,0]
+
+        self.tiles.append(Tile(type,x, y, data[i][j]))
 
   def draw(self):
     for tile in self.tiles:
@@ -41,11 +49,11 @@ class Map():
 
 
 class Tile(pyglet.sprite.Sprite):
-
-  def __init__(self, image, x = 0, y = 0):
+  def __init__(self, image, x = 0, y = 0, passable = 0):
     super(Tile, self).__init__(image)
-    self.x = x;
-    self.y = y + OFFSET_Y;
+    self.x = x
+    self.y = y + OFFSET_Y
+    self.passable = passable
 
 if __name__ == "__main__":
   game = Game() #creates a 'MyGame' instance
