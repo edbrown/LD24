06b7bb424601dd7ca757de77714d97ec4454a639
Lewke <LJGibson@googlemail.com>
2012-08-26 18:17:06 +0100

    Added inventory file proper

---
+++ Inventory.py
@@ -0,0 +1,19 @@
+import pyglet
+
+from definitions import *
+
+class Inventory(pyglet.sprite.Sprite):
+    
+    items = []
+    
+    def __init__(self, image, x, y):
+        super(Inventory, self).__init__(image, x, y)
+
+    def add_item(item):
+        return self.items.append(item)
+
+    def remove_item(self, index):
+        return self.items.remove(item)
+   
+    def item_count(self, item):
+        return items.count(item)
