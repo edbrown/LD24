4daf2067fa70162aa2f013bc5eb40a4a32864f8a
edb <ed.j.brown@googlemail.com>
2012-08-26 15:05:12 +0100

    Pass correct amount of args

--- AnimatedEntity.py
+++ AnimatedEntity.py
@@ -5,4 +5,4 @@ from VisibleEntity import *
 class AnimatedEntity(VisibleEntity):
 
   def __init__(self, image, x, y, passable = 1):
-    super(AnimatedEntity, self).__init__(image, x, y, passable)
+    super(AnimatedEntity, self).__init__(image, x, y)
